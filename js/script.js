9050d9ce853208c08ac64fb936ae1cf4676e322b
JayexDesigns <joaquintouron@gmail.com>
2025-06-02 23:47:26 +0200

    solved bug, applied formatting and changed some styles

--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
-window.addEventListener('load', () => {
-    let color = window.location.search;
+window.addEventListener("load", () => {
+	let color = window.location.search;
 
-    if (color.length === 7 && /[0-9A-F]{6}$/i.test(color.substring(1, 7))) {
-        color = color.substring(1, 7);
-        const body = document.body;
-        const nameTag = document.getElementById("color-name");
-        const codeTag = document.getElementById("color-code");
+	if (color.length === 7 && /[0-9A-F]{6}$/i.test(color.substring(1, 7))) {
+		color = color.substring(1, 7);
+		const body = document.body;
+		const nameTag = document.getElementById("color-name");
+		const codeTag = document.getElementById("color-code");
 
-        const toDec = (hex) => {
-            return parseInt(hex, 16);
-        };
+		const toDec = hex => {
+			return parseInt(hex, 16);
+		};
 
-        body.style.backgroundColor = `#${color}`;
-        nameTag.innerText = ntc.name(`#${color}`)[1];
-        codeTag.innerText = `#${color}`;
+		body.style.backgroundColor = `#${color}`;
+		nameTag.innerText = ntc.name(`#${color}`)[1];
+		codeTag.innerText = `#${color}`;
 
-        let r = toDec(color.substring(0, 2));
-        let g = toDec(color.substring(2, 4));
-        let b = toDec(color.substring(4, 6));
-        if ((r*0.299 + g*0.587 + b*0.114) > 186) {
-            nameTag.style.color = "#000000";
-            codeTag.style.color = "#000000";
-        }
-        else {
-            nameTag.style.color = "#ffffff";
-            codeTag.style.color = "#ffffff";
-        }
-    }
-    else {
-        document.getElementById("alert").style.visibility = "visible";
-    }
+		let r = toDec(color.substring(0, 2));
+		let g = toDec(color.substring(2, 4));
+		let b = toDec(color.substring(4, 6));
+		if (r * 0.299 + g * 0.587 + b * 0.114 > 186) {
+			nameTag.style.color = "#000000";
+			codeTag.style.color = "#000000";
+		} else {
+			nameTag.style.color = "#ffffff";
+			codeTag.style.color = "#ffffff";
+		}
+	} else {
+		document.getElementById("alert").style.visibility = "visible";
+	}
 });
